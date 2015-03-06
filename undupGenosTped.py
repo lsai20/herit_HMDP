767cb059a61c0973215038eb45f8bd703ef3914e
lgai <lgai@login1.(none)>
2015-03-05 20:32:06 -0800

    some edit to undup script, but it's too slow wip

--- undupGenosTped.py
+++ undupGenosTped.py
@@ -4,8 +4,8 @@
 # heritability in HMDP
 # LG
 # unduplicate genotypes while preserving order of strains in phenotype file
-
-
+# really slow, don't use
+ 
 import sys
 
 sys.argv
@@ -28,23 +28,31 @@ keepIndivs = [] # row number of first member in each strain
 currentStrain = ''
 rowNum = -1 # current row in file
 
+print("Now finding individuals to keep in genotype file...")
 # find row id of first indiv in each strain
 with open(strainPhenoFile,'r') as phenoinf:
     for line in phenoinf:
-        rowCount += 1
-        newStrain = (line.split('\s', 3))[2]
+        rowNum += 1
+        newStrain = (line.split(None, 3))[1] # second col is strain
         if newStrain != currentStrain:
-            keepIndivs.append(rowCount)
+            keepIndivs.append(rowNum)
 
+print("Now trimming genotype file...")
+snpcount = 0 # print progress
 # write tped with only one geno column per indiv
-with open(dupGenoFile,'r') as genoinf, open(outputName,'w') as outf:
+outf = open(outputName,'w') # py2.6 can only do one file in with i believe
+with open(dupGenoFile,'r') as genoinf:
     for line in genoinf:
+        snpcount += 1
+        if snpcount % 2000 == 0:
+            print("writing snp %d..." % snpcount)
         snpL = line.strip().split()
-        # first four cols are chr/snp id stuff, rest are genotype for each indiv
+        # first four cols are chr/snp id stuff, rest are genotype for indiv
         undupL = snpL[:4] + [snpL[4:][i] for i in keepIndivs]
         outf.write("\t".join(undupL) + "\n")
 
-
+outf.close()
+print("Done unduplicating")
 
 
 
