eb0b7f97059ee5066fdf38d60ba5558f4b73a6bc
LG <lisagai@wifi-131-179-9-7.host.ucla.edu>
2015-03-05 18:53:16 -0800

    wip simple script to unduplicate genos in right order

---
+++ undupGenosTped.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+
+# heritability in HMDP
+# LG
+# unduplicate genotypes while preserving order of strains in phenotype file
+
+
+import sys
+
+sys.argv
+
+
+def usage():
+    print("Useage: python undupGenosTped.py strainPhenoFile dupGenoFile outputFileName")
+
+if len(sys.argv) != 4:
+    usage()
+    sys.exit()
+
+
+strainPhenoFile = sys.argv[1]   #
+dupGenoFile = sys.argv[2]       # duplicated tped
+outputName = sys.argv[3]
+
+keepIndivs = [] # row number of first member in each strain
+
+currentStrain = ''
+rowNum = -1 # current row in file
+
+# find row id of first indiv in each strain
+with open(strainPhenoFile) as phenoinf:
+    for line in phenoinf:
+        rowCount += 1
+        newStrain = (line.split('\s', 3))[2]
+        if newStrain != currentStrain:
+            keepIndivs.append(rowCount)
+
+# write tped with only one geno column per indiv
+with dupGenoFile as genoinf, open(outputName) as outf:
+    for line in genoinf:
+        snpL = line.strip().split()
+        # first four cols are snp info, rest are genotype for each indiv
+        undupL = snp:[:4] + [snpL[4:][i] for i in keepIndivs]
+        
+
+
+
+
+
+
