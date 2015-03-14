214b06a91879cb43066dc36171f1fed33f758911
LG <lisagai@wifi-131-179-8-145.host.ucla.edu>
2015-03-14 12:49:11 -0700

    script to get first pheno done

--- strainFirstPheno.py
+++ strainFirstPheno.py
@@ -2,10 +2,8 @@
 
 # HMDP heritability
 # LG
-# samples random phenotype from each strain in .pheno file, or uses first phenotype
+# get first indiv in each strain
 # assumes all indiv in strain are together in subsequent rows
-# handles missing values of -9, any capitalization of nan in phenotype (omitted from average)
-# phenotypes where all indiv in phenotype are missing are given value of -9
 
 # input format:
 # strain1_0	strain1	pheno0_value	pheno1_value .... phenoN_value
@@ -16,7 +14,19 @@ import random
 
 
 def usage():
-	print("Usage: python strainFirstPheno.py phenotypeFile outputFile numPhenotypes")
+	print("Usage: python strainFirstPheno.py phenotypeFile outputFile")
+
+if len(sys.argv) != 3:
+    usage()
+    sys.exit()
+
+fileName = sys.argv[1]
+outputName = sys.argv[2]
+
+# check if output file already exists to avoid overwriting
+if os.path.exists(outputName): 
+	print("ERROR: Output file \'%s\' already exists. Please specify a unique name for output." % outputName)
+	sys.exit()
 
 
 def isNonMissingVal(s):
@@ -25,64 +35,26 @@ def isNonMissingVal(s):
 		return False
 	return True
 
-if len(sys.argv) == 4:
-	fileName = sys.argv[1]
-	outputName = sys.argv[2]
-	numPheno = int(sys.argv[3])
-
-else: # wrong number of options
-    usage()
-    sys.exit()
-
-
-# check if output file already exists to avoid overwriting
-if os.path.exists(outputName): 
-	print("ERROR: Output file \'%s\' already exists. Please specify a unique name for output." % outputName)
-	sys.exit()
-
-isDummyStrain = True
-currentStrain = ""
-runningTotals = [0.0] * numPheno # for each pheno, sum of phenotype values
-numIndivs = [0] * len(runningTotals) # for each pheno, number of indiv without missing value
-
+def replaceNan(s):
+	if s.lower() == 'nan':
+		return '-9'
+	else:
+		return s
 
 outf = open(outputName,'w')
+currentStrain = ""
 
 with open(fileName,'r') as inf:
 
 	for line in inf:
 		lineL = line.strip().split()
-
-		# parse new indiv
 		newStrain = lineL[1] # strain is in 2nd col
-		newVals = [floatOrMiss(val) for val in lineL[2:]] # 0 if missing, whatever it is otherwise
-		newIndivs = [isNonMissingVal(val) for val in lineL[2:]] # True/1 if valid, False/0 if missing
-
-		# if we've reached end of current strain
 		if newStrain != currentStrain:
-			if isDummyStrain == False:
-				# compute avg of old strain and write
-				avgPhenos = [str(divideMaybe(x[0],x[1])) for x in zip(runningTotals, numIndivs)]
-				avgLine = currentStrain + "_avg\t" + currentStrain + "\t" +  "\t".join(avgPhenos) + "\n"
-				outf.write(avgLine)
-			
-			else: # else just finished dummy strain
-				isDummyStrain = False
+			# outf.write(line) # just write the unedited line for first indiv
 
-			# reset vals
+			# replace any nan with -9
+			outLineL = [replaceNan(s) for s in lineL]
+			outf.write(" ".join(outLineL) + "\n")
 			currentStrain = newStrain
-			runningTotals = [0.0] * numPheno
-			numIndivs = [0] * len(runningTotals)
-			
 
-		# update with new individual
-		runningTotals = [sum(x) for x in zip(runningTotals, newVals)]
-		numIndivs = [sum(x) for x in zip(numIndivs, newIndivs)]
-
-
-# compute last strain avg after EOF
-avgPhenos = [str(divideMaybe(x[0],x[1])) for x in zip(runningTotals, numIndivs)] 
-avgLine = currentStrain + "_avg\t" + currentStrain + "\t" +  "\t".join(avgPhenos) + "\n"
-outf.write(avgLine)
 outf.close()
-
